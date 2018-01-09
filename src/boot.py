027b1f839a773517cfd9fc414c01e6ff17cba122
Vishnu <devarakonda.vishnu5@gmail.com>
2018-01-08 18:19:50 -0800

    Wifi Connect

--- src/boot.py
+++ src/boot.py
@@ -1,6 +1,4 @@
-from machine import Pin
-
-
+import time
 """
 Pin Labels
 
@@ -18,9 +16,30 @@ D10 = 1;
 
 """
 
+"""
 PD5 = Pin(14,Pin.OUT)
 PD6 = Pin(12,Pin.OUT)
 PD7 = Pin(13,Pin.OUT)
 DP9 = Pin(3,Pin.OUT)
 DP10 = Pin(1,Pin.OUT)
+"""
+
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    
+    if not wlan.isconnected():
+        ID = str(input("Enter SSID: "))
+        Pass = str(input("Enter Pass: "))
+        
+        wlan.active(True)
+        wlan.connect(ID,Pass)
+        deadline = time.ticks_add(time.ticks_ms(),30000)
+        while not(wlan.isconnected() or not (time.ticks_diff(deadline,time.ticks_ms())> 0)):
+            print('Connection to WIFI')
+
+        print('network config:', wlan.ifconfig())
+    else:
+        print('network config:',wlan.ifconfig())
+
 
